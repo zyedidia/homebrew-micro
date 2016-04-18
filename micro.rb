af07bc59503bf2c968b732b8cf609d6a74a22810
Zachary Yedidia <zyedidia@gmail.com>
2016-04-18 09:49:29 -0400

    Update sha

--- micro.rb
+++ micro.rb
@@ -4,7 +4,7 @@ class Micro < Formula
 
     devel do
         url "http://zbyedidia.webfactional.com/micro/binaries/micro-osx.tar.gz"
-        sha256 "616ea85d02abdda50cbfd198b20a9d2790984f10a52b11709f5fdf186822fc72"
+        sha256 "cc244307a0fb6fad9ee8839aebbb6c84aae5b468998a47086e979142a19d8cb8"
     end
 
     def install
