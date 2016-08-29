bcfce1a32b8ae5f4962e6a85d959ff1bb78aa3ee
Zachary Yedidia <zyedidia@gmail.com>
2016-08-29 15:35:32 -0400

    Update for 1.0

--- micro.rb
+++ micro.rb
@@ -1,6 +1,8 @@
 class Micro < Formula
     desc "A modern and intuitive terminal-based text editor"
     homepage "https://github.com/zyedidia/micro"
+    url "https://github.com/zyedidia/micro/releases/download/v1.0/micro-1.0-osx.tar.gz"
+    sha256 "3acccfa2f8bea3656af0c5b4fe0d364de2865587d2fb2458612697fd7540c294"
 
     devel do
         def self.sha256_checksum
@@ -15,3 +17,4 @@ class Micro < Formula
         bin.install Dir["micro"]
     end
 end
+
