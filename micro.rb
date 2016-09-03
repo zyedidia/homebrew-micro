932d877bed018db4c5982a7a5dd55b83d16cf5dd
Zachary Yedidia <zyedidia@gmail.com>
2016-09-03 13:33:46 -0400

    Fix sha256 issue

--- micro.rb
+++ micro.rb
@@ -2,7 +2,7 @@ class Micro < Formula
     desc "A modern and intuitive terminal-based text editor"
     homepage "https://github.com/zyedidia/micro"
     url "https://github.com/zyedidia/micro/releases/download/v1.0.1/micro-1.0.1-osx.tar.gz"
-    sha256 "72fe755732daf7b6b4980182c107f8edda7aa312f005d511750c3b3e70b5c566"
+    sha256 "9961521fc42e48fc8a39ee94fa2b61da97bd49a5ea96f56c8b07881d75216d34"
 
     devel do
         def self.sha256_checksum
