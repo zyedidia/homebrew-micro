20f17b7ea193f4d3c9d2437c28828f302ca0197d
Zachary Yedidia <zyedidia@gmail.com>
2016-09-04 13:37:12 -0400

    Update for 1.0.2

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ This is the Homebrew formula for [micro](https://github.com/zyedidia/micro).
 brew tap zyedidia/micro
 brew install micro
 ```
+
+You can also install the nightly version of micro using `--devel`. Just make sure you remove cached versions brew has or there will be a problem with the checksum.
+
+```
+brew tap zyedidia/micro
+brew install --devel micro
+```
--- micro.rb
+++ micro.rb
@@ -1,8 +1,8 @@
 class Micro < Formula
     desc "A modern and intuitive terminal-based text editor"
     homepage "https://github.com/zyedidia/micro"
-    url "https://github.com/zyedidia/micro/releases/download/v1.0.1/micro-1.0.1-osx.tar.gz"
-    sha256 "9961521fc42e48fc8a39ee94fa2b61da97bd49a5ea96f56c8b07881d75216d34"
+    url "https://github.com/zyedidia/micro/releases/download/v1.0.2/micro-1.0.2-osx.tar.gz"
+    sha256 "2608d2f4fe3c8b8d9517e5e4ce8b3b14647e8b6ff90345aed209396f37d41cdc"
 
     devel do
         def self.sha256_checksum
